2868ba474ae87cfca8893dd8597034bf8313f3ad
Richard Dallaway <richard@dallaway.com>
2012-12-17 17:21:02 +0000

    Initial checkin.

---
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,36 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import common.{Full, Empty}
+import http._
+import code.lib.PamfletUrl
+
+
+class Boot {
+  def boot {
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    // Use HTML5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))
+
+
+
+
+    LiftRules.earlyResponse.append {
+
+      // The first version of the cookbook was written Pamfet.  We map the Pamflet
+      // pages to the corresponding section of the current version of the cookbook:
+      case PamfletUrl(name) => Full(RedirectResponse("/"+name))
+
+      case _ => Empty
+
+    }
+
+
+  }
+}
